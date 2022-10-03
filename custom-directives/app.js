71e50b711438ece34998522ead173c0d2f6b1355
Hakan Durmaz <hakan.durmaz@atmosware.turkcell.com.tr>
2022-10-03 11:29:56 +0300

    - added custom directive with "=" scope

--- custom-directives/app.js
+++ custom-directives/app.js
@@ -45,8 +45,7 @@ myApp.directive("searchResult", function () {
     templateUrl: "directives/searchresult.html",
     replace: true,
     scope: {
-      personName: "@",
-      personAddress: "@"
-    }
+      personObject: "=",
+    },
   };
 });
